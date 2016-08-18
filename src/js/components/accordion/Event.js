f049c998d49887c37869178997dd2daabe8386b9
rook91 <zaqMG3mmG8JMzb$w>
2016-08-18 16:02:06 +0200

    + example odds parser ver beta beta

--- src/js/components/accordion/Event.js
+++ src/js/components/accordion/Event.js
@@ -7,8 +7,37 @@ export default class Event extends React.Component {
         //request for data about match
         //get local file with odds
         //in real code there will be request with id from this.props.id
-        console.log(odds);
-        //display tabs panel
+        // console.log(odds);
+        var oddsNames = {
+            '1x2' : '1X2',
+            '1x2_10M' : '1X2 10min',
+            '1x2_15M' : '1X2 15min',
+            '1x2_30M' : '1X2 30min',
+            '1x2_60M' : '1X2 60min',
+            '1x2_75M' : '1X2 75min',
+            '1x2_H1' : '1X2 First half',
+            '1x2_H2' : '1X2 Second Half'
+        };
+        var a = {
+            "1x2": []
+        };
+        odds.M.forEach(function(pItem){
+            if(pItem['@K'].match('^1x2*')){
+                a['1x2'].push({
+                    id: pItem['@K'],
+                    header: oddsNames[pItem['@K']],
+                    type: 'subtype',
+                    children: pItem.B.map((pItem) => ({
+                        date: pItem['@BTDT'],
+                        bookmakerID: pItem['@I'],
+                        "1": pItem.O[0]['@V'],
+                        "X": pItem.O[1]['@V'],
+                        "2": pItem.O[2]['@V']
+                    }))
+                });
+            }
+        });
+        console.log(a);
     }
 
     render() {
